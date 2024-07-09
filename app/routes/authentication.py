a3ab1426e53c98ff5ee7744082519aee030f2ef9
jackleonard0102 <jackleonard0102@gmail.com>
2024-07-09 01:27:32 -0700

    login company state validation added

--- app/routes/authentication.py
+++ app/routes/authentication.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from app.forms.auth_forms import LoginForm
-from app.models import User, Truck, HarvestRig
+from app.models import User, Truck, HarvestRig, Customer
 from app.extensions import db
 
 auth_bp = Blueprint('authentication', __name__)
@@ -11,11 +11,15 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data.strip()).first()
-        
         if user:
-            print(f"User found: {user.username}")
-            print(f"Password hash: {user.password_hash}")
-        
+            # Fetch the company using company_id
+            company = Customer.query.get(user.company_id)
+
+            # Check if company exists and its status is 'disabled'
+            if company and company.status == 'disable':
+                flash('Your company is not allowed to access this service.')
+                return render_template('login.html', form=form)
+
         if user and user.verify_password(form.password.data.strip()):
             login_user(user)
             return redirect(url_for('main.home'))
